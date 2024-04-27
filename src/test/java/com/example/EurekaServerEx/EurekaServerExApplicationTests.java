e6f2afb043c638432ddddcac8ee6ec289c9b9888
Shrikrushna Hajare <krushnahajare2425@gmail.com>
2024-04-27 11:16:42 +0530

    second commit

---
+++ src/test/java/com/example/EurekaServerEx/EurekaServerExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EurekaServerEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
